3362b3caa6f2ca5e7f1e157a3d291ebe8fe8db8b
jgdevroman <jgdevromank@gmail.com>
2019-05-27 00:26:06 +0900

    full_title_helper added

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @base_title = "SkillChecker"
+    @base_title = "Skill Checker"
   end
 
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
   
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get about" do
